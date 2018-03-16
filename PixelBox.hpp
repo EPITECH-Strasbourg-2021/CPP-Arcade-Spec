0fd89d32d8a2b32ac6a112b6eed3df79ffe0341f
Nicolas Polomack <nipolom@live.fr>
2018-03-16 20:30:28 +0100

    Allow for non-uniform PixelBoxes + Refactoring

--- PixelBox.hpp
+++ PixelBox.hpp
@@ -16,24 +16,22 @@ namespace Arcade {
 	public:
 		PixelBox(size_t height, size_t width, size_t posW,
 			size_t posH);
-		size_t getH() const;
-		size_t getPosH() const;
-		void setH(size_t height);
-		void setPosH(size_t posH);
+		size_t &getHeight() const;
+		size_t &getY() const;
+		void setHeight(size_t height);
+		void setY(size_t y);
 
-		size_t getW() const;
-		size_t getPosW() const;
-		void setW(size_t width);
-		void setPosW(size_t posW);
+		size_t &getWidth() const;
+		size_t &getX() const;
+		void setWidth(size_t width);
+		void setX(size_t x);
 
-		void setColor(Color &color);
-		Color &getColor();
-		void setSprite(void *sprite);
-		void *getSprite();
+		void putPixel(Vect<size_t> pos, Color col);
+		Color &getPixel(Vect<size_t> pos) const;
 
+		const Color *getPixels() const;
 	private:
-		void *_sprite;
-		Color _color;
+		Color *_colorTab;
 		Vect<size_t> _size;
 		Vect<size_t> _pos;
 	};
