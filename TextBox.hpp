b010379ae211a2bd49f5230fc2cf6f591511391a
Nicolas Polomack <nipolom@live.fr>
2018-03-23 22:09:30 +0100

    QoL changes on TextBox

--- TextBox.hpp
+++ TextBox.hpp
@@ -13,21 +13,28 @@
 namespace Arcade {
 	class TextBox {
 	public:
-		TextBox(std::string const &text, size_t h, size_t w,
-			size_t posH, size_t posW);
+		TextBox(std::string const &text, Vect<size_t> size,
+			Vect<size_t> pos);
 		~TextBox() = default;
+
 		const std::string &getValue() const;
 		void setValue(std::string const &text);
 
-		size_t getH() const;
-		size_t getPosH() const;
-		void setH(size_t height);
-		void setPosH(size_t posH);
+		size_t getHeight() const;
+		size_t getY() const;
+		void setHeight(size_t height);
+		void setY(size_t y);
 
-		size_t getW() const;
-		size_t getPosW() const;
-		void setW(size_t width);
-		void setPosW(size_t posW);
+		size_t getWidth() const;
+		size_t getX() const;
+		void setWidth(size_t width);
+		void setX(size_t x);
+
+		Vect<size_t> getPos() const;
+		void setPos(Vect<size_t> pos);
+
+		Vect<size_t> getSize() const;
+		void setSize(Vect<size_t> size);
 
 	private:
 		std::string _value;
