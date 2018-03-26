762c3495904540ae8187cc30f92329e03287ce7d
Nicolas Polomack <nipolom@live.fr>
2018-03-26 16:45:09 +0200

    Written documentation about event-handling in IGraphicLib

--- IGraphicLib.hpp
+++ IGraphicLib.hpp
@@ -25,7 +25,7 @@ namespace Arcade {
 		// Main loop condition
 		virtual bool isOpen() const = 0;
 
-		
+
 		// Closes the window => stop loop (go menu)
 		virtual void closeRenderer() = 0;
 
@@ -34,36 +34,43 @@ namespace Arcade {
 
 		// Clears the screen
 		virtual void clearWindow() = 0;
-		
+
 		// Displays the buffered frame to the screen
 		virtual void refreshWindow() = 0;
-		
+
 		/* Rendering functions */
 		// Draws a PixelBox or the sprite if supported
 		virtual void drawPixelBox(PixelBox &) = 0;
-		
+
 		// Draws a TextBox
 		virtual void drawText(TextBox &) = 0;
 
 
-		/* Events handling */
-		// Gets the last saved event (used in render loop)
-		virtual Keys getLastEvent() = 0;
-		
-		// Saves the event in the Graphics library
+		/* EVENT HANDLING */
+		/* */
+		/* This part can seem complicated but the implementation is quite simple. */
+		/* */
+		/* pollEvent() fetches the events from the user and saves it */
+		/* inside an std::vector<Arcade::Keys> or std::list<Arcade::Keys>. */
+		/* It returns true if at least one new event was added to the list, false otherwise. */
+		/* */
+		/* getLastEvent() returns the first event of the list. (using front() and pop_front() methods) */
+		/* or Arcade::Keys::NONE if the list is empty. */
+		/* */
+		/* clearEvents() clears the list (using clear() method). */
+		/* */
 		virtual bool pollEvents() = 0;
-		
-		// Deletes the last event
+		virtual Keys getLastEvent() = 0;
 		virtual void cleanEvents() = 0;
 
 		
 		/* Context Info */
 		// get the X and Y max of the windows
 		virtual Vect<size_t> getScreenSize() const = 0;
-		
+
 		// get the Y max of the windows
 		virtual int getMaxY() const = 0;
-		
+
 		// get the X max of the windows
 		virtual int getMaxX() const = 0;
 	};
