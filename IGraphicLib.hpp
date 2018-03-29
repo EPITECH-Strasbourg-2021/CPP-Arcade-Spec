0553efe19fd71e73e415f66ef510d52a32e4c756
Arthur Knoepflin <arthur.knoepflin@epitech.eu>
2018-03-29 22:21:10 +0200

    [~] Return type for getMaxY / getMaxX

--- IGraphicLib.hpp
+++ IGraphicLib.hpp
@@ -66,9 +66,9 @@ namespace Arcade {
 		virtual Vect<size_t> getScreenSize() const = 0;
 
 		// get the Y max of the windows
-		virtual int getMaxY() const = 0;
+		virtual size_t getMaxY() const = 0;
 
 		// get the X max of the windows
-		virtual int getMaxX() const = 0;
+		virtual size_t getMaxX() const = 0;
 	};
 };
